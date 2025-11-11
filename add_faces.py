2bb8be3c2e3f3b5039c274aa86564162f92cc103
Sparsh Bansal <sparshbansal787@gmail.com>
2025-11-11 15:27:14 +0530

    frame converted into grayscale

--- add_faces.py
+++ add_faces.py
@@ -1,9 +1,11 @@
 import cv2
 
 video=cv2.VideoCapture(0)
+facedetect=cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
 
 while True:
     ret,frame=video.read()
+    gray=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow("Frame",frame)
     k=cv2.waitKey(1)
     if k==ord('q'):
