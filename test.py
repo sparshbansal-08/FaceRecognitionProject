2b7d18d6cef21510dbe3200b4cf5dc457dc71104
Sparsh Bansal <sparshbansal787@gmail.com>
2025-12-04 22:05:39 +0530

    attendace is taking

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ import pickle
 import numpy as np
 import os
 import csv
+import time
+from datetime import datetime
 
 video = cv2.VideoCapture(0)
 facedetect = cv2.CascadeClassifier('data/haarcascade_frontalface_default.xml')
@@ -30,6 +32,7 @@ box_width = box_x2 - box_x1
 box_height = box_y2 - box_y1
 
 cv2.namedWindow("Frame", cv2.WINDOW_NORMAL) 
+COL_NAMES = ['NAME', 'TIME']
 
 while True:
     ret, frame = video.read()
@@ -43,13 +46,16 @@ while True:
         crop_img = frame[y:y+h, x:x+w, :]
         resized_img = cv2.resize(crop_img, (50, 50)).flatten().reshape(1, -1)
         output = knn.predict(resized_img)
-
-        
+        ts=time.time()
+        date=datetime.fromtimestamp(ts).strftime("%d-%m-%Y")
+        timestamp=datetime.fromtimestamp(ts).strftime("%H:%M-%S")
+        exist=os.path.isfile("Attendance/Attendance_" + date + ".csv")
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 1)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 2)
         cv2.rectangle(frame, (x, y-40), (x+w, y), (50, 50, 255), -1)
         cv2.putText(frame, str(output[0]), (x, y-15), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 1)
         cv2.rectangle(frame, (x, y), (x+w, y+h), (50, 50, 255), 1)
+        attendance=[str(output[0]), str(timestamp)]
 
 
     frame_resized = cv2.resize(frame, (box_width, box_height))
@@ -59,6 +65,19 @@ while True:
     cv2.imshow("Frame", imgBackground)
 
     k = cv2.waitKey(1)
+    if k==ord('o'):
+        
+        if exist:
+            with open("Attendance/Attendance_" + date + ".csv", "+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(attendance)
+            csvfile.close()
+        else:
+            with open("Attendance/Attendance_" + date + ".csv", "+a") as csvfile:
+                writer=csv.writer(csvfile)
+                writer.writerow(COL_NAMES)
+                writer.writerow(attendance)
+            csvfile.close()
     if k == ord('q'):
         break
 
